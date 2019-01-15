05d46fc773a7b45fdccdb9e8873d5351d550f0d9
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-01-15 15:07:37 +0200

    Refs #100740; Use --no-robots in linkchecker

--- src/linksintegrity/linkchecker.sh
+++ src/linksintegrity/linkchecker.sh
@@ -9,7 +9,7 @@ report_location=$2/$(date +"%FT%H%M%S+0000")
 
 mkdir -p $report_location
 
-docker run --rm linkchecker/linkchecker $1 $3 > $report_location/linksintegrity.txt 2>&1
+docker run --rm linkchecker/linkchecker $1 $3 --no-robots > $report_location/linksintegrity.txt 2>&1
 
 echo "Finished getting data for: $1"
 
