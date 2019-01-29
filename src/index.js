af5d1ddae84015b052bb130809149251cd275aea
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-01-29 17:59:59 +0200

    Refs #101376; Wip on switching to garie-plugin

--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
 const garie_plugin = require('garie-plugin')
+const child_process = require('child_process');
+const path = require('path');
 
-function getData_complex(url, options){
-//    console.log(url, options)
-//return {'v1':1, 'v2':22};
-        return {js_events:2, server_events:3, nb_visits:10}
+
+function getResults(file) {
+
+    var regex = RegExp("That's it. ([0-9]+) link[s]? in ([0-9]+) URL[s]? checked. ([0-9]+) warning[s]? found. ([0-9]+) error[s]? found.", 'g');
+
+    var values = regex.exec(file);
+
+    var links = parseInt(values[1]);
+    var errors = parseInt(values[4]);
+    var result = {};
+
+    result['linksintegrity'] = 100 - (100 * errors / links);
+    return result;
 }
 
-function getData_simple(url, options){
-    return {'v1':1, 'v2':22};
-}
+const getData = async (options) => {
+    const { url } = options.url_settings;
+    return new Promise(async (resolve, reject) => {
 
-function getMeasurement_complex(url, data){
-    measurement = []
-    measurement.push({
-        measurement: 'JsEvents/TotalVisits',
-        tags:  url ,
-        fields: { value: data.js_events / data.nb_visits * 100, total_visits: data.nb_visits, sentry_events: data.js_events }
+        try {
+            var { recursion_depth } = options.url_settings;
+            const { reportDir } = options;
+            if (recursion_depth === undefined){
+                recursion_depth = 1
+            }
+            const recursion_str = '-r ' + recursion_depth;
+            const child = child_process.spawn('bash', [path.join(__dirname, './linkchecker.sh'), url, reportDir, recursion_str]);
+
+            const 
+
+            child.stdout.pipe(process.stdout);
+            child.stderr.pipe(process.stderr);
+        } catch (err) {
+            console.log(`Failed to get data for ${url}`, err);
+            reject(`Failed to get data for ${url}`);
+        }
     });
-    measurement.push({
-        measurement: 'ServerEvents/TotalVisits',
-        tags:  url ,
-        fields: { value: data.server_events / data.nb_visits * 100, total_visits: data.nb_visits, sentry_events: data.server_events }
-    });
+};
 
-    return measurement;
 
-}
-
-garie_plugin.init({getData:getData_simple,config:{"cron": "0 */4 * * *",urls:[{url:"www.test.com"}]}});
-
-garie_plugin.init({getData:getData_complex, getMeasurement:getMeasurement_complex, config:{"cron": "0 */4 * * *",urls:[{url:"www.test1.com"}]}});
+garie_plugin.init({getData:getData, getResults: getResults, app_name:'linksintegrity', app_root: path.join(__dirname, '..'), config:{"cron": "0 */4 * * *",urls:[{url:"https://www.eea.europa.eu", recursion_depth:"0"}]}});
